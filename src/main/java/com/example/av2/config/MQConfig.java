dda411f7002eb8cb52bc87a3d239b1735a36d619
36129382024.1M <laurasenac29@gmail.com>
2025-12-04 12:27:31 -0300

    testandinho

--- src/main/java/com/example/av2/config/MQConfig.java
+++ src/main/java/com/example/av2/config/MQConfig.java
@@ -1,13 +1,16 @@
-package com.example.av2.Config;
+package com.example.av2.config;
+
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MQConfig {
+
+
 
     @Bean
     public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
@@ -19,6 +22,7 @@ public class MQConfig {
 
     @Bean
     public MessageConverter jsonMessageConverter() {
-        return new JacksonJsonMessageConverter();
+
+        return new Jackson2JsonMessageConverter();
     }
 }
--- src/main/java/com/example/av2/service/SomaService.java
+++ src/main/java/com/example/av2/service/SomaService.java
@@ -1,4 +1,4 @@
-package com.example.av2.Service;
+package com.example.av2.service;
 import com.example.av2.dto.ListaNumerosDTO;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
